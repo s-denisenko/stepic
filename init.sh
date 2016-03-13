7f70425048e45bb68687423151dcf7ce49da53e0
s-denisenko <denisenkosfedu@gmail.com>
2016-03-13 15:34:02 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,6 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/sites-available/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+
+sudo ln -sf $PWD/etc/hello.py /etc/gunicorn.d/hello.py
+gunicorn -c /etc/gunicorn.d/hello.py hello:app
